97d3612056797719a3b258daa86bdc91dca99278
Cameron <Cameron@dansclan.com>
2022-12-15 09:15:11 -0800

    commit tree 6c743326bf98d4708682bf285d85bc0cf6c4418b

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,31 @@
 class ToysController < ApplicationController
-  wrap_parameters format: []
+    wrap_parameters format: []
 
-  def index
-    toys = Toy.all
-    render json: toys
-  end
+    def index
+        toys = Toy.all
+        render json: toys
+    end
 
-  def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
-  end
+    def create
+        toy = Toy.create(toy_params)
+        render json: toy, status: :created
+    end
 
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
+    def update
+        toy = Toy.find_by(id: params[:id])
+        toy.update(toy_params)
+        render json: toy, status: :accepted
+    end
 
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
+    def destroy
+        toy = Toy.find_by(id: params[:id])
+        toy.destroy
+        head :no_content
+    end
 
-  private
-  
-  def toy_params
-    params.permit(:name, :image, :likes)
-  end
+    private
 
+    def toy_params
+        params.permit(:name, :image, :likes)
+    end
 end
